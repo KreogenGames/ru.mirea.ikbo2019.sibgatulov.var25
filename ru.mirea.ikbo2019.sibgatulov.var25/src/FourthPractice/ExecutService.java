72d51341e56db8292f1f2b01b030f749fec58493
KreogenGames <70753488+KreogenGames@users.noreply.github.com>
2021-03-20 15:43:43 +0300

    Java Fourth

---
+++ ru.mirea.ikbo2019.sibgatulov.var25/src/FourthPractice/ExecutService.java
@@ -0,0 +1,19 @@
+package FourthPractice;
+
+import java.util.concurrent.*;
+
+public class ExecutService {
+    private ExecutorService executorService;
+
+    public ExecutService(int num) throws ExecutionException, InterruptedException{
+        executorService = Executors.newFixedThreadPool(num);
+
+        Callable<String> name = () -> Thread.currentThread().getName();
+
+        for (int i = 0; i < num; i++) {
+            Future result = executorService.submit(name);
+            System.out.println("Поток номер " + (i+1) + " называется " + result.get());
+        }
+        executorService.shutdown();
+    }
+}
---
+++ ru.mirea.ikbo2019.sibgatulov.var25/src/FourthPractice/Test.java
@@ -0,0 +1,9 @@
+package FourthPractice;
+
+import java.util.concurrent.ExecutionException;
+
+public class Test {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutService executService = new ExecutService(2);
+    }
+}
